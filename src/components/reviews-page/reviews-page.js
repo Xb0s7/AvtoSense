d74ea3f0583d81ff37d56adcaefb4b018b0676b6
Velizar Harizanov <harizanovworks@gmail.com>
2020-08-20 14:52:49 +0300

    pagination created

--- src/components/reviews-page/reviews-page.js
+++ src/components/reviews-page/reviews-page.js
@@ -1,34 +1,46 @@
 import React from 'react';
 import Wrapper from '../page-wrapper/wrapper';
 import Review from '../review/review';
-import styles from './reviews-page.module.css'
+import styles from './reviews-page.module.css';
+import ReactPaginate from 'react-paginate';
 class ReviewsPage extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            reviews: []
+            reviewsToRender: [],
+            offset: 0,
+            perPage:5,
+            currentPage:0
         }
     }
    getReviews = async() =>{
         const response = await fetch("http://localhost:9999/api/reviews/");
         const reviews = await response.json();
-        console.log(reviews);
+        const reviewsToMap = reviews.slice(this.state.offset, this.state.offset + this.state.perPage);
+        const reviewsToRender = reviewsToMap.map(review => {
+
+            return <Review key={review._id} {...review}/>
+        })
+        console.log(reviewsToRender);
         this.setState({
-            reviews
+            pageCount: Math.ceil(reviews.length / this.state.perPage),
+            reviewsToRender
         })
     }
+    handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        const offset = selectedPage * this.state.perPage;
 
+        this.setState({
+            currentPage: selectedPage,
+            offset: offset
+        }, () => {
+            this.getReviews()
+        });
+
+    };
     
-    renderReviews() {
-        const {reviews} = this.state;
-
-        return reviews.map((review) => {
-            return (
-                <Review key={review._id} {...review}/>
-            )
-        })
-    }
     componentDidMount() {
         this.getReviews();
     }
@@ -38,8 +50,20 @@ class ReviewsPage extends React.Component {
                 <div className={styles["reviews-page"]}>
                     <div className={styles["reviews-container"]}>
                         <div className={styles.reviews}>
-                        {this.renderReviews()}
-                        </div>
+                            {this.state.reviewsToRender}
+                                <ReactPaginate
+                                previousLabel={"<"}
+                                nextLabel={">"}
+                                breakLabel={"..."}
+                                breakClassName={styles["break-me"]}
+                                pageCount={this.state.pageCount}
+                                marginPagesDisplayed={2}
+                                pageRangeDisplayed={5}
+                                onPageChange={this.handlePageClick}
+                                containerClassName={styles["pagination"]}
+                                subContainerClassName={styles["pages-pagination"]}
+                                activeClassName={styles["active"]}/>
+                         </div>
                     </div>
                 </div>
             </Wrapper>
