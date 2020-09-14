abb667861a86628f2422eb3e633698fad875ae04
Velizar Harizanov <harizanovworks@gmail.com>
2020-09-14 20:32:43 +0300

    CHECK

--- REST API/controllers/user.js
+++ REST API/controllers/user.js
@@ -11,15 +11,15 @@ module.exports = {
 
     post: {
         register: (req, res, next) => {
-            const { username, password } = req.body;
-            models.User.create({ username, password })
+            const { email, password } = req.body;
+            models.User.create({ email, password })
                 .then((createdUser) => res.send(createdUser))
                 .catch(next)
         },
 
         login: (req, res, next) => {
-            const { username, password } = req.body;
-            models.User.findOne({ username })
+            const { email, password } = req.body;
+            models.User.findOne({ email })
                 .then((user) => Promise.all([user, user.matchPassword(password)]))
                 .then(([user, match]) => {
                     if (!match) {
@@ -44,20 +44,5 @@ module.exports = {
                 })
                 .catch(next);
         }
-    },
-
-    put: (req, res, next) => {
-        const id = req.params.id;
-        const { username, password } = req.body;
-        models.User.update({ _id: id }, { username, password })
-            .then((updatedUser) => res.send(updatedUser))
-            .catch(next)
-    },
-
-    delete: (req, res, next) => {
-        const id = req.params.id;
-        models.User.deleteOne({ _id: id })
-            .then((removedUser) => res.send(removedUser))
-            .catch(next)
     }
 };
--- REST API/models/User.js
+++ REST API/models/User.js
@@ -4,11 +4,11 @@ const saltRounds = 10;
 
 const Schema = mongoose.Schema;
 const Model = mongoose.model;
-const { String, Number, Boolean, ObjectId } = Schema.Types;
+const { String, ObjectId } = Schema.Types;
 
 const userSchema = new Schema({
 
-    username: {
+    email: {
         type: String,
         unique: true,
         required: true
