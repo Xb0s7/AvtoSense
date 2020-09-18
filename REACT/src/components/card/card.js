af82dd977afc7a0d05cde7800a166043e8488c34
Velizar Harizanov <harizanovworks@gmail.com>
2020-09-19 00:50:12 +0300

    card and maps component migrated to hooks

--- REACT/src/components/card/card.js
+++ REACT/src/components/card/card.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import styles from './card.module.css'
 import UserContext from '../../utils/context';
-import {Link} from 'react-router-dom';
-class Card extends Component {
-    static contextType = UserContext;
-    render() {
-        let button;
-        if(this.context.loggedIn){
-            button = <Link to="reviews"><span>Reviews</span></Link>
-        }
-        else{
-            button = <Link to="login"><span>Log In</span></Link>
-        }
-        return (
-            <div className={styles.card}>
-                <h2>YOU'RE IN GOOD HANDS</h2>
-                <p>At AvtoSense, your satisfaction is our top priority. If you are already our client and you want to help us improve please Log In leave a review. </p>
-                <div className={styles["schedule-button"]}>
-                    {button}
-                </div>
-            </div>
-        )
+import { Link } from 'react-router-dom';
+const Card = () => {
+    const {loggedIn} = useContext(UserContext);
+    let button;
+    if (loggedIn) {
+        button = <Link to="reviews"><span>Reviews</span></Link>
     }
+    else {
+        button = <Link to="login"><span>Log In</span></Link>
+    }
+    return (
+        <div className={styles.card}>
+            <h2>YOU'RE IN GOOD HANDS</h2>
+            <p>At AvtoSense, your satisfaction is our top priority. If you are already our client and you want to help us improve please Log In leave a review. </p>
+            <div className={styles["schedule-button"]}>
+                {button}
+            </div>
+        </div>
+ )
 }
 
 export default Card;
